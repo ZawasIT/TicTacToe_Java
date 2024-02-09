288e36c605a44227a10a93f54453bc6eaa9fa2ff
ZawasIT <zawadzkiszymonit@gmail.com>
2024-02-09 23:05:13 +0100

    Utworzenie klasy Player, do obsługi graczy.

---
+++ src/main/java/com/example/tictac/Player.java
@@ -0,0 +1,25 @@
+package com.example.tictac;
+
+public class Player {
+    private String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void increamentScore() {
+        score++;
+    }
+
+}
