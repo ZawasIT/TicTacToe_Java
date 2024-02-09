82ebc24a35d9b7e77c270f640418c1c96515ad22
ZawasIT <zawadzkiszymonit@gmail.com>
2024-02-09 23:38:07 +0100

    Okno startowe z podawaniem nazw graczy.

---
+++ src/main/java/com/example/tictac/StartWindowController.java
@@ -0,0 +1,48 @@
+package com.example.tictac;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartWindowController {
+
+    @FXML
+    private TextField playerOTextField;
+
+    @FXML
+    private TextField playerXTextField;
+
+    @FXML
+    private Button startButton;
+
+    private Stage getStage() {
+        return (Stage) startButton.getScene().getWindow();
+    }
+    @FXML
+    void handleStartButton(ActionEvent event) throws IOException {
+
+        if (playerOTextField.getText().isEmpty() || playerXTextField.getText().isEmpty()) {
+            playerXTextField.setPromptText("Wymagane!");
+            playerOTextField.setPromptText("Wymagane!");
+
+            return;
+        }
+        Game.initializePlayer(playerXTextField.getText(), playerOTextField.getText());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ticTacToe_main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Tic Tac Toe Zawas");
+        stage.setScene(scene);
+        stage.show();
+
+        getStage().close();
+    }
+
+}
--- src/main/java/com/example/tictac/TicTacToeApplication.java
+++ src/main/java/com/example/tictac/TicTacToeApplication.java
@@ -10,12 +10,16 @@ import java.io.IOException;
 public class TicTacToeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TicTacToeApplication.class.getResource("ticTacToe_main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TicTacToeApplication.class.getResource("startWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Tic Tac Toe");
+        stage.setTitle("Tic Tac Toe Zawas");
         stage.setScene(scene);
+        stage.setMinWidth(700);
+        stage.setMinHeight(400);
+        stage.setMaxWidth(700);
+        stage.setMaxHeight(400);
+        stage.setResizable(false);
         stage.show();
-        Game.initializePlayer("Agata", "Szymon");
     }
 
     public static void main(String[] args) {
